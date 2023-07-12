c643d8b020983b7993811997667156de01aa1530
Huynh-Duy-Quy <huynhduyquy2001@gmail.com>
2023-07-12 15:57:03 +0700

    12-07-2023 15:56

--- Viesonet_Java6/src/main/java/com/viesonet/entity/Reply.java
+++ Viesonet_Java6/src/main/java/com/viesonet/entity/Reply.java
@@ -2,6 +2,8 @@ package com.viesonet.entity;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,6 +33,7 @@ public class Reply {
 
 	private String replyContent;
 	
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "commentId" )
 	private Comments comment;
--- Viesonet_Java6/src/main/java/com/viesonet/service/FollowService.java
+++ Viesonet_Java6/src/main/java/com/viesonet/service/FollowService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viesonet.dao.FollowDao;
+import com.viesonet.entity.AccountAndFollow;
 import com.viesonet.entity.Follow;
+import com.viesonet.entity.Users;
 
 @Service
 public class FollowService {
@@ -15,5 +17,13 @@ public class FollowService {
 	FollowDao followDao;
 	public List<Follow> getFollowing(String followingId){
 		return followDao.findByFollowingId(followingId);
+	}
+	public AccountAndFollow getFollowingFollower(Users user){
+		AccountAndFollow obj = new AccountAndFollow();
+		obj.setUser(user);
+		obj.setFollowers(followDao.getFollowersById(user));
+		obj.setFollowing(followDao.getFollowingById(user));
+		return obj;
+		
 	}
 }
