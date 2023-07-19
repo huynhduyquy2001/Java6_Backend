fafb1c7cb4483cb5ca8dbfdd2bfcfcf6953e2e04
Lý Tường <lyquoctuong272@gmail.com>
2023-07-19 15:26:07 +0700

    push code layout web 19/07/23

--- Viesonet_Java6/src/main/resources/static/js/postsviolations.js
+++ Viesonet_Java6/src/main/resources/static/js/postsviolations.js
@@ -17,8 +17,15 @@ selectAllCheckbox.addEventListener('click', function() {
 	}
 });
 
-tableCheckboxes.forEach(checkbox => checkbox.addEventListener('click', updateSelectedCount));
-
+tableCheckboxes.forEach(checkbox => checkbox.addEventListener('click', function() {
+	updateSelectedCount();
+	// Hiển thị hoặc ẩn phần tử dựa trên trạng thái của checkbox
+	if (checkbox.checked) {
+		myHidden.style.display = 'block'; // Hiển thị phần tử
+	} else {
+		myHidden.style.display = 'none'; // Ẩn phần tử
+	}
+}));
 function updateSelectedCount() {
 	const selectedCount = document.querySelectorAll('.table-checkbox:checked').length;
 	selectedCountElement.textContent = selectedCount > 0 ? selectedCount : '';
@@ -199,7 +206,15 @@ input.addEventListener("keyup", function(event) {
 				}
 			});
 
-			tableCheckboxes.forEach(checkbox => checkbox.addEventListener('click', updateSelectedCount));
+			tableCheckboxes.forEach(checkbox => checkbox.addEventListener('click', function() {
+				updateSelectedCount();
+				// Hiển thị hoặc ẩn phần tử dựa trên trạng thái của checkbox
+				if (checkbox.checked) {
+					myHidden.style.display = 'block'; // Hiển thị phần tử
+				} else {
+					myHidden.style.display = 'none'; // Ẩn phần tử
+				}
+			}));
 
 			function updateSelectedCount() {
 				const selectedCount = document.querySelectorAll('.table-checkbox:checked').length;
@@ -246,49 +261,49 @@ function deleteViolation() {
 					data: JSON.stringify(listPostId),
 					contentType: "application/json",
 					success: function(data) {
-					/*	var table = document.getElementById("listTable");
-						table.innerHTML = "";
-						data.content.forEach(function(item) {
-							//Chuyển định dạng ngày tháng năm
-							let date = new Date(item[3]);
-							let day = date.getUTCDate().toString().padStart(2, '0');
-							let month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-							let year = date.getUTCFullYear().toString();
-							let dateFormat = `${day}/${month}/${year}`;
-							table.innerHTML += `<tr>
-									<td class="border-bottom-0"><input type="checkbox"
-										value="${item[0]}"
-										class="table-checkbox form-check-input hover-effect">
-									</td>
-									<td class="border-bottom-0">
-									<span class="fw-normal mb-0"
-										style="max-width: 250px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
-									${item[1]}
-									</span>
-									</td>
-									<td class="border-bottom-0"
-										style="max-width: 200px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
-										<span class="fw-normal">${item[2]}</span>
-									</td>
-									<td class="border-bottom-0">
-										<p class="mb-0 fw-normal">${dateFormat}</p>
-									</td>
-									<td class="border-bottom-0 text-center"><a href="#"
-										data-bs-placement="top" title="Xem chi tiết bài viết"
-										th:onclick="detail('${item[0]}')"> <i
-											class="fa-light fa-eye" style="color: #336cce;"></i>
-									</a></td>
-								</tr>`; }); */
-							Swal.fire({
-								position: 'top',
-								icon: 'success',
-								text: 'Xóa bài viết vi phạm thành công!',
-								showConfirmButton: false,
-								timer: 1800
-							})
-							
-							setTimeout(reloadPage, 1800);
-						
+						/*	var table = document.getElementById("listTable");
+							table.innerHTML = "";
+							data.content.forEach(function(item) {
+								//Chuyển định dạng ngày tháng năm
+								let date = new Date(item[3]);
+								let day = date.getUTCDate().toString().padStart(2, '0');
+								let month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+								let year = date.getUTCFullYear().toString();
+								let dateFormat = `${day}/${month}/${year}`;
+								table.innerHTML += `<tr>
+										<td class="border-bottom-0"><input type="checkbox"
+											value="${item[0]}"
+											class="table-checkbox form-check-input hover-effect">
+										</td>
+										<td class="border-bottom-0">
+										<span class="fw-normal mb-0"
+											style="max-width: 250px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
+										${item[1]}
+										</span>
+										</td>
+										<td class="border-bottom-0"
+											style="max-width: 200px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
+											<span class="fw-normal">${item[2]}</span>
+										</td>
+										<td class="border-bottom-0">
+											<p class="mb-0 fw-normal">${dateFormat}</p>
+										</td>
+										<td class="border-bottom-0 text-center"><a href="#"
+											data-bs-placement="top" title="Xem chi tiết bài viết"
+											th:onclick="detail('${item[0]}')"> <i
+												class="fa-light fa-eye" style="color: #336cce;"></i>
+										</a></td>
+									</tr>`; }); */
+						Swal.fire({
+							position: 'top',
+							icon: 'success',
+							text: 'Xóa bài viết vi phạm thành công!',
+							showConfirmButton: false,
+							timer: 1800
+						})
+
+						setTimeout(reloadPage, 1800);
+
 					},
 					error: function(xhr, status, error) {
 						// Xử lý lỗi (nếu có)
@@ -310,7 +325,7 @@ function deleteViolation() {
 }
 
 function reloadPage() {
-            location.reload();
-        }
+	location.reload();
+}
 
 
