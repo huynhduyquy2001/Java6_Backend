3d2cb6d85760118a01dc6c8bc8abbcbefe82d3f7
Vie_Hiu <daynewtran@gmail.com>
2023-07-22 22:22:53 +0700

    10:21 22/07/2023
    Gần xong tìm kiếm (80%) có thêm phân nhỏ gợi ý theo dõi

--- Viesonet_Java6/src/main/java/com/viesonet/controller/ListFollowController.java
+++ Viesonet_Java6/src/main/java/com/viesonet/controller/ListFollowController.java
@@ -9,24 +9,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.viesonet.service.FollowService;
 import com.viesonet.service.SearchService;
+import com.viesonet.service.SessionService;
 
 import jakarta.websocket.server.PathParam;
 
 import com.viesonet.dao.UsersDao;
 import com.viesonet.entity.Users;
+
 @RestController
 public class ListFollowController {
 	@Autowired
 	private SearchService SearchService;
 	@Autowired
 	private UsersDao UsersDao;
-	
+	@Autowired
+	private FollowService followService;
+	@Autowired
+	private SessionService session;
+
 	@GetMapping("/ListFollow")
 	public ModelAndView getHomePage() {
-        ModelAndView modelAndView = new ModelAndView("ListFollow");
-        return modelAndView;
-    }
+		ModelAndView modelAndView = new ModelAndView("ListFollow");
+		return modelAndView;
+	}
+
+	// Lấy thông tin chi tiết các followers
+	@GetMapping("/ListFollower")
+	public List<Users> getFollowersInfoByUserId(@SessionAttribute("id") String userId) {
+		return followService.getFollowersInfoByUserId(session.get("id"));
+	}
+
+	// Lấy thông tin chi tiết các followers
+	@GetMapping("/ListFollowing")
+	public List<Users> getFollowingInfoByUserId(@SessionAttribute("id") String userId) {
+		return followService.getFollowingInfoByUserId(session.get("id"));
+	}
 }
