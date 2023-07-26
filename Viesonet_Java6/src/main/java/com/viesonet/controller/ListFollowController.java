8e5fd4fb95dffa30a4f80420352a3ef6fe1bcafd
Vie_Hiu <daynewtran@gmail.com>
2023-07-26 15:57:48 +0700

    26/07/2023 16:00
    Tìm kiếm với list follow

--- Viesonet_Java6/src/main/java/com/viesonet/controller/ListFollowController.java
+++ Viesonet_Java6/src/main/java/com/viesonet/controller/ListFollowController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -15,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.viesonet.service.FollowService;
 import com.viesonet.service.SearchService;
 import com.viesonet.service.SessionService;
+import com.viesonet.service.UsersService;
 
 import jakarta.websocket.server.PathParam;
 
@@ -32,6 +35,9 @@ public class ListFollowController {
 	@Autowired
 	private SessionService session;
 
+	@Autowired
+	private UsersService UsersService;
+
 	@GetMapping("/ListFollow")
 	public ModelAndView getHomePage() {
 		ModelAndView modelAndView = new ModelAndView("ListFollow");
@@ -39,14 +45,42 @@ public class ListFollowController {
 	}
 
 	// Lấy thông tin chi tiết các followers
-	@GetMapping("/ListFollower")
-	public List<Users> getFollowersInfoByUserId(@SessionAttribute("id") String userId) {
-		return followService.getFollowersInfoByUserId(session.get("id"));
-	}
+//	@GetMapping("/ListFollower")
+//	public List<Users> getFollowersInfoByUserId(@SessionAttribute("id") String userId) {
+//		return followService.getFollowersInfoByUserId(session.get("id"));
+//	}
 
 	// Lấy thông tin chi tiết các followers
-	@GetMapping("/ListFollowing")
-	public List<Users> getFollowingInfoByUserId(@SessionAttribute("id") String userId) {
+	@GetMapping("/ListFollowing1")
+	public List<Users> getFollowingInfoByUserId1(@SessionAttribute("id") String userId) {
 		return followService.getFollowingInfoByUserId(session.get("id"));
+	}
+
+	@GetMapping("/ListFollowing")
+	public List<String> getFollowingInfoByUserId(@SessionAttribute("id") String userId) {
+		return followService.findUserIdsOfFollowing(session.get("id"));
+	}
+
+	@GetMapping("/users")
+	public List<Object> getAllUsers() {
+		return UsersService.findByUserSearchAll();
+	}
+
+	@PostMapping("/follow/{followingId}")
+	public void follow(@PathVariable String followingId) {
+		// Lấy userId của người dùng đăng nhập, bạn có thể thực hiện xác thực để lấy
+		// userId
+		
+		String followerId =session.get("id");
+
+		followService.followUser(followerId, followingId);
+	}
+
+	@DeleteMapping("/unfollow/{followingId}")
+	public void unfollow(@PathVariable String followingId) {
+		// Lấy userId của người dùng đăng nhập, bạn có thể thực hiện xác thực để lấy
+		// userId
+		String followerId = session.get("id");
+		followService.unfollowUser(followerId, followingId);
 	}
 }
