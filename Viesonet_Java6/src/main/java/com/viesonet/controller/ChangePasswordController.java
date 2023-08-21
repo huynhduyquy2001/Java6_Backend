e2c778038a05d47b11ae40c629b5e0c953f5f3a4
Lý Tường <lyquoctuong272@gmail.com>
2023-08-21 12:36:28 +0700

    .

--- Viesonet_Java6/src/main/java/com/viesonet/controller/ChangePasswordController.java
+++ Viesonet_Java6/src/main/java/com/viesonet/controller/ChangePasswordController.java
@@ -16,21 +16,15 @@ import org.springframework.web.servlet.ModelAndView;
 import com.viesonet.AuthConfig;
 import com.viesonet.entity.Accounts;
 import com.viesonet.service.AccountsService;
-import com.viesonet.service.SessionService;
 
 @RestController
 public class ChangePasswordController {
 	@Autowired
-	private SessionService sessionService;
-
-	@Autowired
 	private AccountsService accountsService;
-	
+
 	@Autowired
 	private AuthConfig authConfig;
 
-
-	
 	@GetMapping("/changepassword")
 	public ModelAndView getChangePasswordPage() {
 		ModelAndView modelAndView = new ModelAndView("ChangePassword");
@@ -40,28 +34,30 @@ public class ChangePasswordController {
 	@PostMapping("/doimatkhau")
 	public ResponseEntity<?> doimatkhau(@RequestBody Map<String, Object> data, Authentication authentication) {
 		Accounts account = authConfig.getLoggedInAccount(authentication);
-	    String matKhau = (String) data.get("matKhau");
-	    String matKhauMoi = (String) data.get("matKhauMoi");
-	    String matKhauXacNhan = (String) data.get("matKhauXacNhan");
-	    
-	    String hashedNewPassword = authConfig.passwordEncoder().encode(matKhauMoi);
-	    Accounts accounts = accountsService.findByPhoneNumber(account.getPhoneNumber());	    
-	    PasswordEncoder passwordEncoder = authConfig.passwordEncoder();
-	    
-	    if (passwordEncoder.matches(matKhau, accounts.getPassword())) {
-	        if (passwordEncoder.matches(matKhauMoi, accounts.getPassword())) {
-	            return ResponseEntity.ok().body(Collections.singletonMap("message", "Mật khẩu mới không được giống mật khẩu cũ"));
-	        }if(!matKhauMoi.equalsIgnoreCase(matKhauXacNhan)) {
-	        	return ResponseEntity.ok().body(Collections.singletonMap("message", "Mật khẩu và mật khẩu xác nhận không khớp"));
-	        }
-	        else {
-	        	account.setPassword(hashedNewPassword);
-	        	accountsService.save(accounts);     
-	        	return ResponseEntity.ok().build();
-	        }
-	    } else {
-	        return ResponseEntity.ok().body(Collections.singletonMap("message", "Mật khẩu cũ không trùng khớp"));
-	    }
+		String matKhau = (String) data.get("matKhau");
+		String matKhauMoi = (String) data.get("matKhauMoi");
+		String matKhauXacNhan = (String) data.get("matKhauXacNhan");
+
+		String hashedNewPassword = authConfig.passwordEncoder().encode(matKhauMoi);
+		Accounts accounts = accountsService.findByPhoneNumber(account.getPhoneNumber());
+		PasswordEncoder passwordEncoder = authConfig.passwordEncoder();
+
+		if (passwordEncoder.matches(matKhau, accounts.getPassword())) {
+			if (passwordEncoder.matches(matKhauMoi, accounts.getPassword())) {
+				return ResponseEntity.ok()
+						.body(Collections.singletonMap("message", "Mật khẩu mới không được giống mật khẩu cũ"));
+			}
+			if (!matKhauMoi.equalsIgnoreCase(matKhauXacNhan)) {
+				return ResponseEntity.ok()
+						.body(Collections.singletonMap("message", "Mật khẩu và mật khẩu xác nhận không khớp"));
+			} else {
+				account.setPassword(hashedNewPassword);
+				accountsService.save(accounts);
+				return ResponseEntity.ok().build();
+			}
+		} else {
+			return ResponseEntity.ok().body(Collections.singletonMap("message", "Mật khẩu cũ không trùng khớp"));
+		}
 	}
 
 }
